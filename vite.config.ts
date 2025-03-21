142286c5b78ad288d2f04e8356f819861cfc73ad
coderzzx <coderzzx@coderzzxMacBook-Air.local>
2025-03-21 17:09:29 +0800

    Mod -> vite -> 打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,20 @@ export default defineConfig({
     alias: {
       '@': '/src'
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
+        entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
+        assetFileNames: '[ext]/[name]-[hash].[ext]' // 资源文件像 字体，图片等
+      }
+    },
+    terserOptions: {
+      compress: {
+        drop_console: true, // 打包时删除console
+        drop_debugger: true // 打包时删除debugger
+      }
+    }
   }
 });
