e662b2ccdef42f9f0b3497c0a0a2e88ca168d46f
coderzzx <coderzzx@coderzzxMacBook-Air.local>
2025-03-21 17:44:44 +0800

    Mod -> vite -> 打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,13 @@ export default defineConfig({
       output: {
         chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
         entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
-        assetFileNames: '[ext]/[name]-[hash].[ext]' // 资源文件像 字体，图片等
+        assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
+        manualChunks(id) {
+          if (id.indexOf('node_modules') !== -1) {
+            // 拆分第三方包为 一个单独的 chunk 文件
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
       }
     },
     terserOptions: {
