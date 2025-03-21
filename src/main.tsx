f022b1b76619ddd916ea1997f84055523d6e538e
coderzzx <coderzzx@coderzzxMacBook-Air.local>
2025-03-21 17:15:44 +0800

    Mod -> version -> 降低版本

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 
 import 'normalize.css';
 import '@/assets/css/index.css';
-import '@ant-design/v5-patch-for-react-19';
 
 import RootApp from './RootApp';
 import '@/locales'; // 国际化
