9178b074f0c2c1e38dc98876ddbb1e3ea6ceeef1
coderzzx <coderzzx@coderzzxMacBook-Air.local>
2025-03-27 15:58:28 +0800

    Fix -> watermark -> 修复切换语言水印不正确

--- src/components/RootWatermark/RootWatermark.tsx
+++ src/components/RootWatermark/RootWatermark.tsx
@@ -9,7 +9,7 @@ interface IProps {
 
 const RootWatermark: FC<IProps> = ({ children }) => {
   const { userInfo, language } = useAppSelector((state) => state.user, useAppShallowEqual);
-  return <Watermark content={language === 'zh' ? userInfo?.cnName : userInfo?.enName}>{children}</Watermark>;
+  return <Watermark content={language === 'zhCN' ? userInfo?.cnName : userInfo?.enName}>{children}</Watermark>;
 };
 
 export default memo(RootWatermark);
