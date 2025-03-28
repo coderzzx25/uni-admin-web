892a480a430c3dfe90655edc09c3cfb7c583000b
coderzzx <coderzzx@coderzzxMacBook-Air.local>
2025-03-28 17:03:59 +0800

    Mod -> vite -> 打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
       '@': '/src'
     }
   },
+  // 根据环境设置日志级别
+  logLevel: mode === 'production' ? 'warn' : 'info',
+
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
-        chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
-        entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
-        assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        assetFileNames: '[ext]/[name]-[hash].[ext]',
         manualChunks(id) {
           if (id.indexOf('node_modules') !== -1) {
-            // 拆分第三方包为 一个单独的 chunk 文件
             return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
         }
@@ -26,9 +27,9 @@ export default defineConfig({
     },
     terserOptions: {
       compress: {
-        drop_console: true, // 打包时删除console
-        drop_debugger: true // 打包时删除debugger
+        drop_console: mode === 'production', // 生产环境才移除 console
+        drop_debugger: mode === 'production' // 生产环境才移除 debugger
       }
     }
   }
-});
+}));
